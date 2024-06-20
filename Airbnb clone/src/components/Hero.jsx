bc6cfd5e13e3788e8f744f356167e59978b06125
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-20 18:52:39 +0300

    minor

--- Airbnb clone/src/components/Hero.jsx
+++ Airbnb clone/src/components/Hero.jsx
@@ -6,6 +6,9 @@ function Hero(){
     return(
         <div className="hero">
             <img src={hero} alt="Hero Section"className="hero-icon"/>
+            <h1 className="hero-header">Online Experiences</h1>
+            <p className="hero-text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
         </div>
     )
 }
