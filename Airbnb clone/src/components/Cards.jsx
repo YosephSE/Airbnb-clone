61798a21dbc4e9ea8ae444eb525a63fa7d57f7e7
YosephSE <88291669+YosephSE@users.noreply.github.com>
2024-06-20 23:25:54 +0300

    updated

--- Airbnb clone/src/components/Cards.jsx
+++ Airbnb clone/src/components/Cards.jsx
@@ -6,23 +6,37 @@ import rate from "../assets/rate.png";
 function Cards() {
   return (
     <div className="cards">
-      <Card img={card1} span="5.0 (6) . Online" text="Life Lessons with Katie Zaferes" price="From $136 / person"/>
-      <Card img={card2} span="5.0 (6) . Online" text="Learn Wedding Photography" price="From $125 / person"/>
-      <Card img={card3} span="5.0 (6) . Norway" text="Group Mountain Biking" price="From $50 / person"/>
+      <Card
+        img={card1}
+        span="5.0 (6) . Online"
+        text="Life Lessons with Katie Zaferes"
+        price="From $136 / person"
+      />
+      <Card
+        img={card2}
+        span="5.0 (6) . Online"
+        text="Learn Wedding Photography"
+        price="From $125 / person"
+      />
+      <Card
+        img={card3}
+        span="5.0 (6) . Norway"
+        text="Group Mountain Biking"
+        price="From $50 / person"
+      />
     </div>
   );
 }
-function Card({img, span, text, price}){
-  return(
+function Card({ img, span, text, price }) {
+  return (
     <div className="card">
-        <img src={img} alt="Card 1" className="card-img"/>
-        <img src={rate} alt="Rate" className="rate"/>
-        <span>{span}</span>
-        <p>{text}</p>
-        <p>{price}</p>
-      </div>
-      
-  )
+      <img src={img} alt="Card 1" className="card-img" />
+      <img src={rate} alt="Rate" className="rate" />
+      <span>{span}</span>
+      <p>{text}</p>
+      <p>{price}</p>
+    </div>
+  );
 }
 
 export default Cards;
